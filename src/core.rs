a2bce65378452721ba679653c0135de24f0ec76a
idctag <ochir.code@gmail.com>
2026-09-08 23:18:21 +0800

    rework core logic

--- src/core.rs
+++ src/core.rs
@@ -9,19 +9,27 @@ impl UserTickets {
     pub fn new() -> Self {
         Self {
             tickets: Vec::new(),
-            next_id: 1,
+            next_id: 0,
         }
     }
 
-    pub fn list(&self) {
-        for ticket in &self.tickets {
-            println!("{ticket}")
-        }
-    }
-
-    pub fn add_ticket(&mut self, title: String, description: String) {
+    pub fn add_ticket(&mut self, title: String, description: String) -> u32 {
+        let id = self.next_id;
         self.next_id += 1;
         let new_ticket = Ticket::new(self.next_id, title, description);
         self.tickets.push(new_ticket);
+        id
+    }
+
+    pub fn tickets(&self) -> &[Ticket] {
+        &self.tickets
+    }
+
+    pub fn find(&self, id: u32) -> Option<&Ticket> {
+        self.tickets.iter().find(|ticket| ticket.id() == id)
+    }
+
+    pub fn find_mud(&mut self, id: u32) -> Option<&mut Ticket> {
+        self.tickets.iter_mut().find(|ticket| ticket.id() == id)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,33 @@
 use todo_cli::core::UserTickets;
 
 fn main() {
-    let mut tickets = UserTickets::new();
-    tickets.add_ticket(
+    let mut t = UserTickets::new();
+    t.add_ticket(
         "grocery".to_string(),
         "1. carrot, 2. cabbage, 3. beef".to_string(),
     );
 
-    tickets.add_ticket(
+    t.add_ticket(
         "ticket_cli".to_string(),
         "finish the engine logics".to_string(),
     );
 
-    tickets.add_ticket(
+    t.add_ticket(
         "ticket_cli".to_string(),
         "add cli navigation feature".to_string(),
     );
 
-    tickets.add_ticket(
+    t.add_ticket(
         "ticket_cli".to_string(),
         "add cloud storage system".to_string(),
     );
 
-    tickets.add_ticket(
+    t.add_ticket(
         "ticket_cli".to_string(),
         "user authentication system".to_string(),
     );
 
-    tickets.list();
+    for ticket in t.tickets() {
+        println!("{ticket}")
+    }
 }
