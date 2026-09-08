ffe6e86752ccec9dec70fdbd2a7598b6ee914d8e
idctag <ochir.code@gmail.com>
2026-09-08 23:27:47 +0800

    code fixes and tests

--- src/core.rs
+++ src/core.rs
@@ -9,14 +9,14 @@ impl UserTickets {
     pub fn new() -> Self {
         Self {
             tickets: Vec::new(),
-            next_id: 0,
+            next_id: 1,
         }
     }
 
     pub fn add_ticket(&mut self, title: String, description: String) -> u32 {
         let id = self.next_id;
         self.next_id += 1;
-        let new_ticket = Ticket::new(self.next_id, title, description);
+        let new_ticket = Ticket::new(id, title, description);
         self.tickets.push(new_ticket);
         id
     }
@@ -31,5 +31,38 @@ impl UserTickets {
 
     pub fn find_mud(&mut self, id: u32) -> Option<&mut Ticket> {
         self.tickets.iter_mut().find(|ticket| ticket.id() == id)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use crate::ticket::TicketStatus;
+
+    use super::*;
+
+    #[test]
+    fn new_ticket_starts_as_pending() {
+        let ticket = Ticket::new(1, "Learn Rust", "Study Ownership");
+        assert_eq!(ticket.status(), TicketStatus::Pending)
+    }
+
+    #[test]
+    fn adding_ticket_returns_id() {
+        let mut tickets = UserTickets::new();
+        let first_id = tickets.add_ticket("First".to_string(), "Description".to_string());
+        let second_id = tickets.add_ticket("Second".to_string(), "Description".to_string());
+        assert_eq!(first_id, 1);
+        assert_eq!(second_id, 2)
+    }
+
+    #[test]
+    fn can_find_ticket_by_id() {
+        let mut tickets = UserTickets::new();
+        let id = tickets.add_ticket("Ticket".to_string(), "Description".to_string());
+
+        let ticket = tickets.find(id).expect("should exist");
+
+        assert_eq!(ticket.title(), "Ticket".to_string())
     }
 }
